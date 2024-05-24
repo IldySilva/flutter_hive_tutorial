18f893d2a97df3f1254b0c597ada585a447a138f
ildeberto <ildeberto34@gmail.com>
2024-05-24 16:56:19 +0100

    add SliverAppBar

---
+++ lib/components/note_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hive_tutorial/services/date_extensions.dart';
+
+import '../models/note.dart';
+
+class NoteTile extends StatefulWidget {
+  NoteTile({super.key, required this.note});
+
+  Note note;
+  @override
+  State<NoteTile> createState() => _NoteTileState();
+}
+
+class _NoteTileState extends State<NoteTile> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 300))..forward();
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1)
+        .animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    super.initState();
+  }
+
+  double containerWidth = 400;
+  Offset containerOffset = const Offset(0, 0);
+
+  @override
+  void dispose() {
+ _animationController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Card(
+          surfaceTintColor: Colors.white,
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              tileColor: Colors.white,
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (widget.note.description != null) Text(widget.note.description ?? ""),
+                  Text(
+                    widget.note.dateTime.getDayAndHour(),
+                    style: const TextStyle(color: Colors.grey, fontSize: 12),
+                  )
+                ],
+              ),
+              title: Text(widget.note.title),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hive_tutorial/services/date_extensions.dart';
 import 'package:flutter_hive_tutorial/services/notes_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'components/note_tile.dart';
 import 'models/note.dart';
 
 void main() async {
@@ -27,82 +29,62 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: SafeArea(
-          child: Scaffold(
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: const Color(0xff427dde),
-              onPressed: () {
-                NotesService.insert(
-                  Note(title: "Olá", dateTime: DateTime.now(), description: "Olá Mundo"),
-                );
-              },
-              child: const Icon(
-                Icons.add,
-                color: Colors.white,
-              ),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 30),
-                  Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        child: Text(
-                          "My Notes",
-                          style: TextStyle(fontSize: 45, color: Colors.grey.shade600, fontWeight: FontWeight.w300),
-                        ),
-                      )),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-                      Row(
-                        children: [
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
-                        ],
-                      )
-                    ],
-                  ),
-                  Expanded(
-                    child: ValueListenableBuilder(
-                      builder: (context, box, _) {
-                        var list = box.values.toList().reversed.toList();
-                        return ListView.builder(
-                            itemCount: list.length,
-                            itemBuilder: (context, index) {
-                              Note note = list[index];
-
-                              return Card(
-                                surfaceTintColor: Colors.white,
-                                color: Colors.white,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ListTile(
-                                    tileColor: Colors.white,
-                                    subtitle: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        if (note.description != null) Text(note.description ?? ""),
-                                        Text(note.dateTime.getDayAndHour(),style: const TextStyle(color: Colors.grey,fontSize: 12),)
-                                      ],
-                                    ),
-                                    title: Text(note.title),
-                                  ),
-                                ),
-                              );
-                            });
-                      },
-                      valueListenable: Hive.box<Note>("notes").listenable(),
-                    ),
-                  )
-                ],
-              ),
+            child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: const Color(0xff427dde),
+            onPressed: () {
+              NotesService.insert(
+                Note(title: "Olá", dateTime: DateTime.now(), description: "Olá Mundo"),
+              );
+            },
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
             ),
           ),
-        ));
+          body: CustomScrollView(slivers: [
+            SliverAppBar(
+              pinned: true,
+              floating: false,
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(100),
+                child: AppBar(
+                  leading: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.menu),
+                  ),
+                  actions: [
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+                  ],
+                ),
+              ),
+              expandedHeight: 170.0,
+
+              flexibleSpace: Center(
+                child: Text(
+                  "My Notes",
+                  style: TextStyle(fontSize: 45,color: Colors.grey.shade600, fontWeight: FontWeight.w300),  ),
+              ),
+            ),
+            ValueListenableBuilder(
+                valueListenable: Hive.box<Note>("notes").listenable(),
+                builder: (context, box, _) {
+                  var list = box.values.toList().reversed.toList();
+
+                  return SliverList(
+
+                    delegate: SliverChildBuilderDelegate(
+
+                      (_, int index) {
+                        Note note = list[index];
+                        return NoteTile(note: note);
+                      },
+                      childCount: list.length,
+                    ),
+                  );
+                }),
+          ]),
+        )));
   }
 }
